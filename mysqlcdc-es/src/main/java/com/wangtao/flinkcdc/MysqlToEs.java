0a97e0efb4189211c840a5e6869eff092a212556
wangtao <wangtao970503@foxmail.com>
2024-08-29 21:55:09 +0800

    增加说明

--- mysqlcdc-es/src/main/java/com/wangtao/flinkcdc/MysqlToEs.java
+++ mysqlcdc-es/src/main/java/com/wangtao/flinkcdc/MysqlToEs.java
@@ -12,6 +12,26 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  */
 public class MysqlToEs {
 
+    /**
+     * startupOptions
+     * StartupOptions.initial(): 默认值, 全量(表中已有数据)加增量binlog
+     * 全量读取阶段没有锁，可以并行读取，增量监听binlog时并行度会自动变成单一并行度，以保证binlog顺序性
+     * StartupOptions.latest(): 从最新的binlog位置监听, 不会读取表里现有数据
+     * StartupOptions.earliest(): 从binlog最早的位置开始读取, 不会读取表里现有数据，
+     * 若binlog被清理，意味着读取的数据就会不全
+     *
+     * serverId
+     * 由于全量读取阶段时，是无锁模式，单一分片读取最后一条数据和读取此时的binlog位置不是原子性操作，所以需要把这个空隙的
+     * 变化应用进来，这个动作是读取binlog的，所以有多少个并行度就需要有多少个serverId
+     * 值为一个整数或者整数范围，不设置默认会从5400和6400之间生成一个随机数
+     *
+     * 返回的JSON值
+     * op: SQL操作
+     * r: 不是来自binlog
+     * c: insert
+     * u: update
+     * d: delete
+     */
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // 设置 3s 的 checkpoint 间隔
