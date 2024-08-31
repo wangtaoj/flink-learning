1e32a0ecf36625bd25d26fafa4b7bce4fe029c66
wangtao <wangtao970503@foxmail.com>
2024-08-31 15:21:15 +0800

    binlog监听示例

--- mysqlcdc-es/src/main/java/com/wangtao/flinkcdc/MysqlBinlog.java
+++ mysqlcdc-es/src/main/java/com/wangtao/flinkcdc/MysqlBinlog.java
@@ -5,12 +5,17 @@ import com.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.kafka.connect.json.DecimalFormat;
+import org.apache.kafka.connect.json.JsonConverterConfig;
+
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author wangtao
  * Created at 2024-08-27
  */
-public class MysqlToEs {
+public class MysqlBinlog {
 
     /**
      * startupOptions
@@ -37,22 +42,36 @@ public class MysqlToEs {
         // 设置 3s 的 checkpoint 间隔
         env.enableCheckpointing(3000);
 
+        // 底层debezium参数配置
+        Properties debeziumProperties = new Properties();
+        //debeziumProperties.put("decimal.handling.mode", "string");
+
+        Map<String, Object> customConverterConfigs = Map.of(
+                // Decimal类型使用数字而不是base64编码
+                JsonConverterConfig.DECIMAL_FORMAT_CONFIG, DecimalFormat.NUMERIC.name()
+        );
+
         // MySQL CDC依赖flink table api
         MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
                 .hostname("127.0.0.1")
                 .port(3306)
                 .databaseList("test")
-                .tableList("test.t2") // databasename.tablename
+                .tableList("test.mysqlcdc") // databasename.tablename
                 .username("root")
                 .password("123456")
-                .deserializer(new JsonDebeziumDeserializationSchema()) // 将 SourceRecord 转换为 JSON 字符串
-                .startupOptions(StartupOptions.latest()) // 从最新的binlog位置监听, 不会读取历史数据
-                .serverId("5401-5404") // 模拟slave serverId, 读取阶段设置并行度为4, 范围配置需要>=4
+                .serverTimeZone("Asia/Shanghai")
+                // 模拟slave serverId, 读取阶段设置并行度为4, 范围配置需要>=4
+                .serverId("5401-5404")
+                // 读取表中全量数据, 并监听增量数据
+                .startupOptions(StartupOptions.initial())
+                .debeziumProperties(debeziumProperties)
+                // 将 SourceRecord 转换为 JSON 字符串
+                .deserializer(new JsonDebeziumDeserializationSchema(false, customConverterConfigs))
                 .build();
         env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
                 .setParallelism(4)
                 .print()
                 .setParallelism(1); // 为了保证顺序，消费节点设置并行度为1
-        env.execute("MysqlToEs");
+        env.execute("MySQL Binlog");
     }
 }
